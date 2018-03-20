0e8a2909661d6cb745db8dae98e39611d813a29c
Danilo Policarpo <danilo.policarpo@concrete.com.br>
2018-03-20 11:24:49 -0300

    fix dependencias

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,7 @@ require 'appium_lib'
 require 'pry'
 require 'appium_console'
 
-caps = Appium.load_appium_txt file: File.expand_path('./../../../appium.txt', __FILE__), verbose: true
+caps = Appium.load_appium_txt file:
+File.expand_path('./../../../appium.txt', __FILE__), verbose: true
 Appium::Driver.new(caps, true)
 Appium.promote_appium_methods Object
